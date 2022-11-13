d448ccdf289906d0de875d2d75d65dc179b83174
Justin Salcedo <nitsuj162114@gmail.com>
2022-11-12 23:21:23 -0800

    New content: 11/13/2022 (Sunday)

--- schedule.js
+++ schedule.js
@@ -13,10 +13,11 @@
 
 // const SATURDAY = "2022/11/5"
 // const SUNDAY = "2022/11/6"
-const MONDAY = "2022/11/7"
-const TUESDAY = "2022/11/8"
-const WEDNESDAY = "2022/11/9"
-const THURSDAY = "2022/11/10"
+const MONDAY = "2022/11/14"
+const TUESDAY = "2022/11/15"
+const WEDNESDAY = "2022/11/16"
+const THURSDAY = "2022/11/17"
+//
 const FRIDAY = "2022/11/11"
 const SATURDAY = "2022/11/12"
 const SUNDAY = "2022/11/13"
@@ -86,56 +87,68 @@ const MY_SCHEDULE = [
         start: new Date(`${SUNDAY} 8:00`),
         end: new Date(`${SUNDAY} 8:15`)
     },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
-    // {
-    //     name: "",
-    //     start: new Date(`${}`),
-    //     end: new Date(`${}`)
-    // },
+    {
+        name: "Meditate",
+        start: new Date(`${SUNDAY} 8:15`),
+        end: new Date(`${SUNDAY} 8:30`)
+    },
+    {
+        name: "Laundry",
+        start: new Date(`${SUNDAY} 8:30`),
+        end: new Date(`${SUNDAY} 8:45`)
+    },
+    {
+        name: "Zeeth 0.0",
+        start: new Date(`${SUNDAY} 8:45`),
+        end: new Date(`${SUNDAY} 9:45`)
+    },
+    {
+        name: "Breakfast",
+        start: new Date(`${SUNDAY} 9:45`),
+        end: new Date(`${SUNDAY} 10:00`)
+    },
+    {
+        name: "Behind!",
+        start: new Date(`${SUNDAY} 10:00`),
+        end: new Date(`${SUNDAY} 11:00`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${SUNDAY} 11:00`),
+        end: new Date(`${SUNDAY} 11:30`)
+    },
+    {
+        name: "Laundry*",
+        start: new Date(`${SUNDAY} 11:30`),
+        end: new Date(`${SUNDAY} 11:45`)
+    },
+    {
+        name: "Slot",
+        start: new Date(`${SUNDAY} 12:00`),
+        end: new Date(`${SUNDAY} 17:00`)
+    },
+    {
+        name: "Shower",
+        start: new Date(`${SUNDAY} 17:00`),
+        end: new Date(`${SUNDAY} 18:00`)
+    },
+    {
+        name: "Go to bed",
+        start: new Date(`${SUNDAY} 19:00`),
+        end: new Date(`${MONDAY} 5:00`)
+    },
+
+    // Monday
+    {
+        name: "Get ready",
+        start: new Date(`${MONDAY} 5:00`),
+        end: new Date(`${MONDAY} 6:00`)
+    },
+    {
+        name: "Work",
+        start: new Date(`${MONDAY} 6:00`),
+        end: new Date(`${MONDAY} 2:30`)
+    },
     // {
     //     name: "",
     //     start: new Date(`${}`),
