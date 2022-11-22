0688f93edb7bd312b97b92a988ede41080e7743c
Justin Salcedo <nitsuj162114@gmail.com>
2022-11-21 21:28:55 -0800

    New content: 11/22 (Tuesday)

--- schedule.js
+++ schedule.js
@@ -24,73 +24,6 @@ const SATURDAY = "2022/11/26"
 const MY_SCHEDULE = [
     {
         name: "Libellus",
-        start: new Date(`${SUNDAY} 19:45`),
-        end: new Date(`${SUNDAY} 20:00`)
-    },
-    {
-        name: "Shower",
-        start: new Date(`${SUNDAY} 20:00`),
-        end: new Date(`${SUNDAY} 21:00`)
-    },
-    {
-        name: "Sleep",
-        start: new Date(`${SUNDAY} 22:00`),
-        end: new Date(`${MONDAY} 5:00`)
-    },
-
-    // MONDAY
-    {
-        name: "Get ready",
-        start: new Date(`${MONDAY} 5:00`),
-        end: new Date(`${MONDAY} 6:00`)
-    },
-    {
-        name: "Work",
-        start: new Date(`${MONDAY} 6:00`),
-        end: new Date(`${MONDAY} 14:30`)
-    },
-    {
-        name: "Bank visit",
-        start: new Date(`${MONDAY} 14:30`),
-        end: new Date(`${MONDAY} 14:45`)
-    },
-    {
-        name: "Drive",
-        start: new Date(`${MONDAY} 14:45`),
-        end: new Date(`${MONDAY} 15:15`)
-    },
-    {
-        name: "Eat & Setup",
-        start: new Date(`${MONDAY} 15:15`),
-        end: new Date(`${MONDAY} 15:30`)
-    },
-    {
-        name: "IP Reading",
-        start: new Date(`${MONDAY} 15:30`),
-        end: new Date(`${MONDAY} 16:00`)
-    },
-    {
-        name: "IP Discussion",
-        start: new Date(`${MONDAY} 16:00`),
-        end: new Date(`${MONDAY} 17:00`)
-    },
-    {
-        name: "CT Knowledge",
-        start: new Date(`${MONDAY} 17:30`),
-        end: new Date(`${MONDAY} 19:00`)
-    },
-    {
-        name: "CT Discussion",
-        start: new Date(`${MONDAY} 19:00`),
-        end: new Date(`${MONDAY} 20:00`)
-    },
-    {
-        name: "IP Project",
-        start: new Date(`${MONDAY} 20:00`),
-        end: new Date(`${MONDAY} 21:00`)
-    },
-    {
-        name: "Libellus",
         start: new Date(`${MONDAY} 21:00`),
         end: new Date(`${MONDAY} 21:15`)
     },
@@ -102,13 +35,80 @@ const MY_SCHEDULE = [
     {
         name: "Sleep",
         start: new Date(`${MONDAY} 22:00`),
-        end: new Date(`${TUESDAY} 8:00`)
+        end: new Date(`${TUESDAY} 5:00`)
     },
 
-    // Tuesday
+    // MONDAY
     {
         name: "Get ready",
         start: new Date(`${TUESDAY} 5:00`),
         end: new Date(`${TUESDAY} 6:00`)
+    },
+    {
+        name: "Work",
+        start: new Date(`${TUESDAY} 6:00`),
+        end: new Date(`${TUESDAY} 14:30`)
+    },
+    {
+        name: "Drive",
+        start: new Date(`${TUESDAY} 14:30`),
+        end: new Date(`${TUESDAY} 15:00`)
+    },
+    {
+        name: "Shopping",
+        start: new Date(`${TUESDAY} 15:00`),
+        end: new Date(`${TUESDAY} 15:30`)
+    },
+    {
+        name: "Eat & Setup",
+        start: new Date(`${TUESDAY} 15:30`),
+        end: new Date(`${TUESDAY} 15:45`)
+    },
+    {
+        name: "CT Discussion",
+        start: new Date(`${TUESDAY} 16:00`),
+        end: new Date(`${TUESDAY} 16:15`)
+    },
+    {
+        name: "CT Knowledge check",
+        start: new Date(`${TUESDAY} 16:15`),
+        end: new Date(`${TUESDAY} 17:45`)
+    },
+    {
+        name: "IP Discussion",
+        start: new Date(`${TUESDAY} 18:00`),
+        end: new Date(`${TUESDAY} 19:00`)
+    },
+    {
+        name: "IP Project",
+        start: new Date(`${TUESDAY} 19:00`),
+        end: new Date(`${TUESDAY} 20:00`)
+    },
+    {
+        name: "CT Lab",
+        start: new Date(`${TUESDAY} 20:00`),
+        end: new Date(`${TUESDAY} 21:00`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${TUESDAY} 21:00`),
+        end: new Date(`${TUESDAY} 21:15`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${TUESDAY} 21:15`),
+        end: new Date(`${TUESDAY} 21:30`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${TUESDAY} 22:00`),
+        end: new Date(`${WEDNESDAY} 5:00`)
+    },
+
+    // Wednesday
+    {
+        name: "Get ready",
+        start: new Date(`${WEDNESDAY} 5:00`),
+        end: new Date(`${WEDNESDAY} 6:00`)
     },
 ]
