ccdbef65fbc75bfcebf29f8c184429ed0946ff50
Justin Salcedo <nitsuj162114@gmail.com>
2022-11-25 10:53:17 -0800

    New content: 11/25 (Today, Friday)

--- schedule.js
+++ schedule.js
@@ -24,77 +24,77 @@ const SATURDAY = "2022/11/26"
 const MY_SCHEDULE = [
     {
         name: "Sleep",
-        start: new Date(`${WEDNESDAY} 23:45`),
-        end: new Date(`${THURSDAY} 11:00`)
+        start: new Date(`${THURSDAY} 11:45`),
+        end: new Date(`${FRIDAY} 9:00`)
     },
     {
         name: "Meditate",
-        start: new Date(`${THURSDAY} 11:00`),
-        end: new Date(`${THURSDAY} 12:00`)
+        start: new Date(`${FRIDAY} 9:00`),
+        end: new Date(`${FRIDAY} 9:30`)
     },
     {
         name: "Breakfast",
-        start: new Date(`${THURSDAY} 12:00`),
-        end: new Date(`${THURSDAY} 12:30`)
+        start: new Date(`${FRIDAY} 9:30`),
+        end: new Date(`${FRIDAY} 10:00`)
     },
     {
         name: "Libellus App",
-        start: new Date(`${THURSDAY} 12:30`),
-        end: new Date(`${THURSDAY} 16:30`)
+        start: new Date(`${FRIDAY} 10:00`),
+        end: new Date(`${FRIDAY} 16:30`)
     },
     {
         name: "Earth's center",
-        start: new Date(`${THURSDAY} 16:30`),
-        end: new Date(`${THURSDAY} 17:00`)
+        start: new Date(`${FRIDAY} 16:30`),
+        end: new Date(`${FRIDAY} 17:00`)
     },
     {
         name: "To the Moon",
-        start: new Date(`${THURSDAY} 17:00`),
-        end: new Date(`${THURSDAY} 17:30`)
+        start: new Date(`${FRIDAY} 17:00`),
+        end: new Date(`${FRIDAY} 17:30`)
     },
     {
         name: "Swim in Ness Lake",
-        start: new Date(`${THURSDAY} 17:30`),
-        end: new Date(`${THURSDAY} 18:00`)
+        start: new Date(`${FRIDAY} 17:30`),
+        end: new Date(`${FRIDAY} 18:00`)
     },
     {
         name: "Climb the Everest",
-        start: new Date(`${THURSDAY} 18:00`),
-        end: new Date(`${THURSDAY} 18:30`)
+        start: new Date(`${FRIDAY} 18:00`),
+        end: new Date(`${FRIDAY} 18:30`)
     },
     {
         name: "Fight Sparta",
-        start: new Date(`${THURSDAY} 18:30`),
-        end: new Date(`${THURSDAY} 19:00`)
+        start: new Date(`${FRIDAY} 18:30`),
+        end: new Date(`${FRIDAY} 19:00`)
     },
     {
         name: "IP Discussion",
-        start: new Date(`${THURSDAY} 19:00`),
-        end: new Date(`${THURSDAY} 19:30`)
+        start: new Date(`${FRIDAY} 19:00`),
+        end: new Date(`${FRIDAY} 19:30`)
     },
     {
         name: "CT Discussion",
-        start: new Date(`${THURSDAY} 19:30`),
-        end: new Date(`${THURSDAY} 20:00`)
+        start: new Date(`${FRIDAY} 19:30`),
+        end: new Date(`${FRIDAY} 20:00`)
     },
     {
         name: "Dinner & CT Lab",
-        start: new Date(`${THURSDAY} 20:00`),
-        end: new Date(`${THURSDAY} 21:00`)
+        start: new Date(`${FRIDAY} 20:00`),
+        end: new Date(`${FRIDAY} 21:00`)
     },
     {
         name: "Libellus",
-        start: new Date(`${THURSDAY} 21:00`),
-        end: new Date(`${THURSDAY} 21:15`)
+        start: new Date(`${FRIDAY} 21:00`),
+        end: new Date(`${FRIDAY} 21:15`)
     },
     {
         name: "Setup",
-        start: new Date(`${THURSDAY} 21:15`),
-        end: new Date(`${THURSDAY} 21:30`)
+        start: new Date(`${FRIDAY} 21:15`),
+        end: new Date(`${FRIDAY} 21:30`)
     },
     {
         name: "Sleep",
-        start: new Date(`${THURSDAY} 22:00`),
-        end: new Date(`${FRIDAY} 8:00`)
+        start: new Date(`${FRIDAY} 22:00`),
+        end: new Date(`${SATURDAY} 8:00`)
     },
 ]
