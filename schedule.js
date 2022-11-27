86471956049a4478d68cc4dbf9967ced9fc84b47
Justin Salcedo <nitsuj162114@gmail.com>
2022-11-26 22:45:07 -0800

    Fixed bug: loadContent() now assigns previous, next, and current tasks
    Not only reassigns the previous task to the current one, but also reassigns the next task to the current one first.
    New content: 11/27 (Sunday)

--- schedule.js
+++ schedule.js
@@ -11,90 +11,107 @@
 //     end: new Date(`${}`)
 // },
 
-// const SATURDAY = "2022/11/5"
-const SUNDAY = "2022/11/20"
-const MONDAY = "2022/11/21"
-const TUESDAY = "2022/11/22"
-const WEDNESDAY = "2022/11/23"
-const THURSDAY = "2022/11/24"
-const FRIDAY = "2022/11/25"
 const SATURDAY = "2022/11/26"
-// const SUNDAY = "2022/11/27"
+const SUNDAY = "2022/11/27"
+const MONDAY = "2022/11/28"
+const TUESDAY = "2022/11/29"
+const WEDNESDAY = "2022/11/30"
+const THURSDAY = "2022/12/01"
+const FRIDAY = "2022/12/02"
+// const SATURDAY = "2022/12/03"
+// const SUNDAY = "2022/12/04"
 
 const MY_SCHEDULE = [
     {
         name: "Sleep",
-        start: new Date(`${THURSDAY} 11:45`),
-        end: new Date(`${FRIDAY} 9:00`)
+        start: new Date(`${SATURDAY} 22:00`),
+        end: new Date(`${SUNDAY} 8:00`)
     },
+
+    // Sunday
     {
         name: "Meditate",
-        start: new Date(`${FRIDAY} 9:00`),
-        end: new Date(`${FRIDAY} 9:30`)
-    },
-    {
-        name: "Breakfast",
-        start: new Date(`${FRIDAY} 9:30`),
-        end: new Date(`${FRIDAY} 10:00`)
-    },
-    {
-        name: "Libellus App",
-        start: new Date(`${FRIDAY} 10:00`),
-        end: new Date(`${FRIDAY} 16:30`)
-    },
-    {
-        name: "Earth's center",
-        start: new Date(`${FRIDAY} 16:30`),
-        end: new Date(`${FRIDAY} 17:00`)
-    },
-    {
-        name: "To the Moon",
-        start: new Date(`${FRIDAY} 17:00`),
-        end: new Date(`${FRIDAY} 17:30`)
-    },
-    {
-        name: "Swim in Ness Lake",
-        start: new Date(`${FRIDAY} 17:30`),
-        end: new Date(`${FRIDAY} 18:00`)
-    },
-    {
-        name: "Climb the Everest",
-        start: new Date(`${FRIDAY} 18:00`),
-        end: new Date(`${FRIDAY} 18:30`)
-    },
-    {
-        name: "Fight Sparta",
-        start: new Date(`${FRIDAY} 18:30`),
-        end: new Date(`${FRIDAY} 19:00`)
-    },
-    {
-        name: "IP Discussion",
-        start: new Date(`${FRIDAY} 19:00`),
-        end: new Date(`${FRIDAY} 19:30`)
-    },
-    {
-        name: "CT Discussion",
-        start: new Date(`${FRIDAY} 19:30`),
-        end: new Date(`${FRIDAY} 20:00`)
-    },
-    {
-        name: "Dinner & CT Lab",
-        start: new Date(`${FRIDAY} 20:00`),
-        end: new Date(`${FRIDAY} 21:00`)
-    },
-    {
-        name: "Libellus",
-        start: new Date(`${FRIDAY} 21:00`),
-        end: new Date(`${FRIDAY} 21:15`)
+        start: new Date(`${SUNDAY} 8:00`),
+        end: new Date(`${SUNDAY} 8:30`)
     },
     {
         name: "Setup",
-        start: new Date(`${FRIDAY} 21:15`),
-        end: new Date(`${FRIDAY} 21:30`)
+        start: new Date(`${SUNDAY} 8:30`),
+        end: new Date(`${SUNDAY} 8:45`)
+    },
+    {
+        name: "Laundry & Cardboard",
+        start: new Date(`${SUNDAY} 8:45`),
+        end: new Date(`${SUNDAY} 9:15`)
+    },  
+    {
+        name: "IP Knowledge check",
+        start: new Date(`${SUNDAY} 9:15`),
+        end: new Date(`${SUNDAY} 9:30`)
+    },
+    {
+        name: "IP Reading",
+        start: new Date(`${SUNDAY} 9:30`),
+        end: new Date(`${SUNDAY} 10:00`)
+    },
+    {
+        name: "IP Discussion",
+        start: new Date(`${SUNDAY} 10:00`),
+        end: new Date(`${SUNDAY} 11:00`)
+    },
+    {
+        name: "Shower",
+        start: new Date(`${SUNDAY} 11:00`),
+        end: new Date(`${SUNDAY} 12:00`)
+    },
+    {
+        name: "Drive",
+        start: new Date(`${SUNDAY} 12:30`),
+        end: new Date(`${SUNDAY} 13:15`)
+    },
+    {
+        name: "Visit Aunt",
+        start: new Date(`${SUNDAY} 13:15`),
+        end: new Date(`${SUNDAY} 16:00`)
+    },
+    {
+        name: "Drive",
+        start: new Date(`${SUNDAY} 16:00`),
+        end: new Date(`${SUNDAY} 16:45`)
+    },
+    {
+        name: "CT Knowledge check",
+        start: new Date(`${SUNDAY} 17:00`),
+        end: new Date(`${SUNDAY} 18:00`)
+    },
+    {
+        name: "CT Discussion",
+        start: new Date(`${SUNDAY} 18:00`),
+        end: new Date(`${SUNDAY} 19:00`)
+    },
+    {
+        name: "Dinner",
+        start: new Date(`${SUNDAY} 19:00`),
+        end: new Date(`${SUNDAY} 19:15`)
+    },
+    {
+        name: "Learn Perl",
+        start: new Date(`${SUNDAY} 19:15`),
+        end: new Date(`${SUNDAY} 20:30`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${SUNDAY} 20:30`),
+        end: new Date(`${SUNDAY} 21:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${SUNDAY} 21:30`),
+        end: new Date(`${SUNDAY} 21:45`)
     },
     {
         name: "Sleep",
-        start: new Date(`${FRIDAY} 22:00`),
-        end: new Date(`${SATURDAY} 8:00`)
+        start: new Date(`${SUNDAY} 22:00`),
+        end: new Date(`${MONDAY} 5:00`)
     },
 ]
