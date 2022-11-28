427055971176af4ea472d5d5ef8ed4cd1cf4a5de
Justin Salcedo <nitsuj162114@gmail.com>
2022-11-27 21:51:12 -0800

    New content: 11/27 (Monday)

--- schedule.js
+++ schedule.js
@@ -23,88 +23,6 @@ const FRIDAY = "2022/12/02"
 
 const MY_SCHEDULE = [
     {
-        name: "Sleep",
-        start: new Date(`${SATURDAY} 22:00`),
-        end: new Date(`${SUNDAY} 8:00`)
-    },
-
-    // Sunday
-    {
-        name: "Meditate",
-        start: new Date(`${SUNDAY} 8:00`),
-        end: new Date(`${SUNDAY} 8:30`)
-    },
-    {
-        name: "Setup",
-        start: new Date(`${SUNDAY} 8:30`),
-        end: new Date(`${SUNDAY} 8:45`)
-    },
-    {
-        name: "Laundry & Cardboard",
-        start: new Date(`${SUNDAY} 8:45`),
-        end: new Date(`${SUNDAY} 9:15`)
-    },  
-    {
-        name: "IP Knowledge check",
-        start: new Date(`${SUNDAY} 9:15`),
-        end: new Date(`${SUNDAY} 9:30`)
-    },
-    {
-        name: "IP Reading",
-        start: new Date(`${SUNDAY} 9:30`),
-        end: new Date(`${SUNDAY} 10:00`)
-    },
-    {
-        name: "IP Discussion",
-        start: new Date(`${SUNDAY} 10:00`),
-        end: new Date(`${SUNDAY} 11:00`)
-    },
-    {
-        name: "Shower",
-        start: new Date(`${SUNDAY} 11:00`),
-        end: new Date(`${SUNDAY} 12:00`)
-    },
-    {
-        name: "Drive",
-        start: new Date(`${SUNDAY} 12:30`),
-        end: new Date(`${SUNDAY} 13:15`)
-    },
-    {
-        name: "Visit Aunt",
-        start: new Date(`${SUNDAY} 13:15`),
-        end: new Date(`${SUNDAY} 16:00`)
-    },
-    {
-        name: "Drive",
-        start: new Date(`${SUNDAY} 16:00`),
-        end: new Date(`${SUNDAY} 16:45`)
-    },
-    {
-        name: "CT Knowledge check",
-        start: new Date(`${SUNDAY} 17:00`),
-        end: new Date(`${SUNDAY} 18:00`)
-    },
-    {
-        name: "CT Discussion",
-        start: new Date(`${SUNDAY} 18:00`),
-        end: new Date(`${SUNDAY} 19:00`)
-    },
-    {
-        name: "Dinner",
-        start: new Date(`${SUNDAY} 19:00`),
-        end: new Date(`${SUNDAY} 19:15`)
-    },
-    {
-        name: "Learn Perl",
-        start: new Date(`${SUNDAY} 19:15`),
-        end: new Date(`${SUNDAY} 20:30`)
-    },
-    {
-        name: "Libellus",
-        start: new Date(`${SUNDAY} 20:30`),
-        end: new Date(`${SUNDAY} 21:00`)
-    },
-    {
         name: "Setup",
         start: new Date(`${SUNDAY} 21:30`),
         end: new Date(`${SUNDAY} 21:45`)
@@ -112,6 +30,93 @@ const MY_SCHEDULE = [
     {
         name: "Sleep",
         start: new Date(`${SUNDAY} 22:00`),
+        end: new Date(`${MONDAY} 4:00`)
+    },
+
+    // Monday
+    {
+        name: "Get ready",
+        start: new Date(`${MONDAY} 4:00`),
         end: new Date(`${MONDAY} 5:00`)
+    },
+    {
+        name: "Work",
+        start: new Date(`${MONDAY} 5:00`),
+        end: new Date(`${MONDAY} 8:00`)
+    },
+    {
+        name: "Drive",
+        start: new Date(`${MONDAY} 8:00`),
+        end: new Date(`${MONDAY} 8:30`)
+    },
+    {
+        name: "Learn Perl",
+        start: new Date(`${MONDAY} 8:30`),
+        end: new Date(`${MONDAY} 9:00`)
+    },
+    {
+        name: "Work",
+        start: new Date(`${MONDAY} 9:00`),
+        end: new Date(`${MONDAY} 15:00`)
+    },
+    {
+        name: "Call Aerotek & W2",
+        start: new Date(`${MONDAY} 15:00`),
+        end: new Date(`${MONDAY} 15:15`)
+    },
+    {
+        name: "Gather ID Docs",
+        start: new Date(`${MONDAY} 15:15`),
+        end: new Date(`${MONDAY} 15:30`)
+    },
+    {
+        name: "Shower",
+        start: new Date(`${MONDAY} 15:30`),
+        end: new Date(`${MONDAY} 16:30`)
+    },
+    {
+        name: "Passport appointment",
+        start: new Date(`${MONDAY} 16:30`),
+        end: new Date(`${MONDAY} 16:45`)
+    },
+    {
+        name: "FAFSA Form",
+        start: new Date(`${MONDAY} 16:45`),
+        end: new Date(`${MONDAY} 17:00`)
+    },
+    {
+        name: "CT Knowledge check",
+        start: new Date(`${MONDAY} 17:00`),
+        end: new Date(`${MONDAY} 18:00`)
+    },
+    {
+        name: "CT Discussion",
+        start: new Date(`${MONDAY} 18:00`),
+        end: new Date(`${MONDAY} 19:00`)
+    },
+    {
+        name: "Dinner",
+        start: new Date(`${MONDAY} 19:00`),
+        end: new Date(`${MONDAY} 19:15`)
+    },
+    {
+        name: "Plan SNT",
+        start: new Date(`${MONDAY} 19:15`),
+        end: new Date(`${MONDAY} 20:30`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${MONDAY} 20:30`),
+        end: new Date(`${MONDAY} 21:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${MONDAY} 21:30`),
+        end: new Date(`${MONDAY} 21:45`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${MONDAY} 22:00`),
+        end: new Date(`${TUESDAY} 5:00`)
     },
 ]
