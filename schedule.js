bc966a9963f3e06c038b4cea5d99ef4938405eba
Justin Salcedo <nitsuj162114@gmail.com>
2022-12-04 11:19:42 -0800

    New content 12/04 - 05 (Sunday and Monday)

--- schedule.js
+++ schedule.js
@@ -22,91 +22,188 @@ const FRIDAY = "2022/12/09"
 // const SUNDAY = "2022/12/11"
 
 const MY_SCHEDULE = [
-    // {
-    //     name: "Setup",
-    //     start: new Date(`${MONDAY} 21:30`),
-    //     end: new Date(`${MONDAY} 21:45`)
-    // },
-    // {
-    //     name: "Sleep",
-    //     start: new Date(`${MONDAY} 22:00`),
-    //     end: new Date(`${TUESDAY} 4:00`)
-    // },
-
-    // Monday
-    {
-        name: "Love time",
-        start: new Date(`${SATURDAY} 13:00`),
-        end: new Date(`${SATURDAY} 14:00`)
-    },
-    {
-        name: "Lunch",
-        start: new Date(`${SATURDAY} 14:00`),
-        end: new Date(`${SATURDAY} 14:30`)
-    },
-    {
-        name: "Change address",
-        start: new Date(`${SATURDAY} 14:30`),
-        end: new Date(`${SATURDAY} 14:45`)
-    },
-    {
-        name: "Cut Bonsai",
-        start: new Date(`${SATURDAY} 14:45`),
-        end: new Date(`${SATURDAY} 15:00`)
-    },
-    {
-        name: "Gather pendrives",
-        start: new Date(`${SATURDAY} 15:00`),
-        end: new Date(`${SATURDAY} 15:15`)
-    },
-    {
-        name: "Reboot Rossana",
-        start: new Date(`${SATURDAY} 15:45`),
-        end: new Date(`${SATURDAY} 16:30`)
-    },
-    {
-        name: "CT Knowledge check",
-        start: new Date(`${SATURDAY} 16:30`),
-        end: new Date(`${SATURDAY} 18:00`)
-    },
-    {
-        name: "CT Discussion",
-        start: new Date(`${SATURDAY} 18:00`),
-        end: new Date(`${SATURDAY} 18:30`)
-    },
-    {
-        name: "IP Project",
-        start: new Date(`${SATURDAY} 18:30`),
-        end: new Date(`${SATURDAY} 19:30`)
-    },
-    {
-        name: "IP Discussion",
-        start: new Date(`${SATURDAY} 19:30`),
-        end: new Date(`${SATURDAY} 20:00`)
-    },
-    {
-        name: "Shower",
-        start: new Date(`${SATURDAY} 20:30`),
-        end: new Date(`${SATURDAY} 21:30`)
-    },
     {
         name: "Libellus",
-        start: new Date(`${SATURDAY} 21:30`),
-        end: new Date(`${SATURDAY} 21:45`)
+        start: new Date(`${SUNDAY} 2:00`),
+        end: new Date(`${SUNDAY} 2:30`)
     },
     {
         name: "Setup",
-        start: new Date(`${SATURDAY} 21:45`),
-        end: new Date(`${SATURDAY} 22:00`)
-    },
-    {
-        name: "Watch movie",
-        start: new Date(`${SATURDAY} 22:00`),
-        end: new Date(`${SUNDAY} 0:00`)
+        start: new Date(`${SUNDAY} 2:30`),
+        end: new Date(`${SUNDAY} 2:45`)
     },
     {
         name: "Sleep",
-        start: new Date(`${SUNDAY} 0:00`),
-        end: new Date(`${SUNDAY} 8:00`)
-    }
+        start: new Date(`${SUNDAY} 2:45`),
+        end: new Date(`${SUNDAY} 10:30`)
+    },
+
+    // Sunday
+    {
+        name: "Meditate",
+        start: new Date(`${SUNDAY} 10:30`),
+        end: new Date(`${SUNDAY} 11:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${SUNDAY} 11:00`),
+        end: new Date(`${SUNDAY} 11:15`)
+    },
+    {
+        name: "Letter",
+        start: new Date(`${SUNDAY} 11:30`),
+        end: new Date(`${SUNDAY} 12:00`)
+    },
+    {
+        name: "Breakfast",
+        start: new Date(`${SUNDAY} 12:00`),
+        end: new Date(`${SUNDAY} 12:15`)
+    },
+    {
+        name: "CT Knowledge check",
+        start: new Date(`${SUNDAY} 12:15`),
+        end: new Date(`${SUNDAY} 14:00`)
+    },
+    {
+        name: "CT Discussion",
+        start: new Date(`${SUNDAY} 14:00`),
+        end: new Date(`${SUNDAY} 14:30`)
+    },
+    {
+        name: "IP Project",
+        start: new Date(`${SUNDAY} 14:30`),
+        end: new Date(`${SUNDAY} 16:00`)
+    },
+    {
+        name: "CT Lab",
+        start: new Date(`${SUNDAY} 16:00`),
+        end: new Date(`${SUNDAY} 17:15`)
+    },
+    {
+        name: "Reboot Rossana",
+        start: new Date(`${SUNDAY} 17:30`),
+        end: new Date(`${SUNDAY} 18:30`)
+    },
+    {
+        name: "Refactor Libellus",
+        start: new Date(`${SUNDAY} 18:30`),
+        end: new Date(`${SUNDAY} 19:30`)
+    },
+    {
+        name: "Social networking",
+        start: new Date(`${SUNDAY} 19:30`),
+        end: new Date(`${SUNDAY} 20:00`)
+    },
+    // {
+    //     name: "Libellus API",
+    //     start: new Date(`${SUNDAY} 18:30`),
+    //     end: new Date(`${SUNDAY} 19:30`)
+    // },
+    {
+        name: "Plan SNT",
+        start: new Date(`${SUNDAY} 20:00`),
+        end: new Date(`${SUNDAY} 21:00`)
+    },
+    // {
+    //     name: "Snacks and Labels",
+    //     start: new Date(`${SUNDAY} 20:00`),
+    //     end: new Date(`${SUNDAY} 21:00`)
+    // },
+    {
+        name: "Watch movie",
+        start: new Date(`${SUNDAY} 21:00`),
+        end: new Date(`${SUNDAY} 23:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${SUNDAY} 23:00`),
+        end: new Date(`${SUNDAY} 23:15`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${SUNDAY} 23:15`),
+        end: new Date(`${MONDAY} 6:45`)
+    },
+
+    // Monday
+    {
+        name: "Work",
+        start: new Date(`${MONDAY} 7:00`),
+        end: new Date(`${MONDAY} 13:00`)
+    },
+    {
+        name: "Submit FAFSA form",
+        start: new Date(`${MONDAY} 13:00`),
+        end: new Date(`${MONDAY} 13:45`)
+    },
+    {
+        name: "Withdraw",
+        start: new Date(`${MONDAY} 13:45`),
+        end: new Date(`${MONDAY} 14:30`)
+    },
+    {
+        name: "Immigration services",
+        start: new Date(`${MONDAY} 15:00`),
+        end: new Date(`${MONDAY} 16:00`)
+    },
+    {
+        name: "Online shopping",    // Remember to buy: watch, USB pendrives, nutcracker, keyboard, monitor cable
+        start: new Date(`${MONDAY} 16:00`),
+        end: new Date(`${MONDAY} 17:00`)
+    },
+    {
+        name: "IP Knowledge check",
+        start: new Date(`${MONDAY} 17:00`),
+        end: new Date(`${MONDAY} 17:15`)
+    },
+    {
+        name: "IP Reading",
+        start: new Date(`${MONDAY} 17:15`),
+        end: new Date(`${MONDAY} 17:45`)
+    },
+    {
+        name: "CT Knowledge",
+        start: new Date(`${MONDAY} 18:00`),
+        end: new Date(`${MONDAY} 19:00`)
+    },
+    {
+        name: "CT Discussion",
+        start: new Date(`${MONDAY} 19:00`),
+        end: new Date(`${MONDAY} 20:00`)
+    },
+    // {
+    //     name: "Printer setup",
+    //     start: new Date(`${MONDAY} `),
+    //     end: new Date(`${MONDAY} `)
+    // },
+    // {
+    //     name: "Libellus server",
+    //     start: new Date(`${MONDAY} `),
+    //     end: new Date(`${MONDAY} `)
+    // },
+    {
+        name: "Dinner",
+        start: new Date(`${MONDAY} 20:00`),
+        end: new Date(`${MONDAY} 20:30`)
+    },
+    {
+        name: "Plan SNT",
+        start: new Date(`${MONDAY} 20:30`),
+        end: new Date(`${MONDAY} 21:30`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${MONDAY} 21:30`),
+        end: new Date(`${MONDAY} 21:45`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${MONDAY} 21:45`),
+        end: new Date(`${MONDAY} 22:00`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${MONDAY} 22:00`),
+        end: new Date(`${TUESDAY} 6:45`)
+    },
 ]
