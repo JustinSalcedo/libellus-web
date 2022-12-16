f5848566eabd8bc05ba8f684a4017ab67e74463b
Justin Salcedo <nitsuj162114@gmail.com>
2022-12-15 22:49:01 -0800

    New content: 12/16 (Friday)

--- schedule.js
+++ schedule.js
@@ -11,78 +11,103 @@
 //     end: new Date(`${}`)
 // },
 
-const SATURDAY = "2022/12/10"
-const SUNDAY = "2022/12/11"
+// const SATURDAY = "2022/12/10"
+// const SUNDAY = "2022/12/11"
 const MONDAY = "2022/12/12"
 const TUESDAY = "2022/12/13"
 const WEDNESDAY = "2022/12/14"
 const THURSDAY = "2022/12/15"
 const FRIDAY = "2022/12/16"
-// const SATURDAY = "2022/12/17"
-// const SUNDAY = "2022/12/18"
+const SATURDAY = "2022/12/17"
+const SUNDAY = "2022/12/18"
 
 const MY_SCHEDULE = [
     {
+        name: "Call Brandon",
+        start: new Date(`${THURSDAY} 21:00`),
+        end: new Date(`${THURSDAY} 23:00`)
+    },
+    {
         name: "Libellus",
-        start: new Date(`${WEDNESDAY} 23:15`),
-        end: new Date(`${WEDNESDAY} 23:30`)
+        start: new Date(`${THURSDAY} 23:00`),
+        end: new Date(`${THURSDAY} 23:15`)
     },
     {
         name: "Setup",
-        start: new Date(`${WEDNESDAY} 23:30`),
-        end: new Date(`${THURSDAY} 0:00`)
+        start: new Date(`${THURSDAY} 23:30`),
+        end: new Date(`${FRIDAY} 0:00`)
     },
     {
         name: "Sleep",
-        start: new Date(`${THURSDAY} 0:00`),
-        end: new Date(`${THURSDAY} 6:45`)
+        start: new Date(`${FRIDAY} 0:00`),
+        end: new Date(`${FRIDAY} 6:45`)
     },
 
     // Wednesday
     {
         name: "Meditate",
-        start: new Date(`${THURSDAY} 6:45`),
-        end: new Date(`${THURSDAY} 7:00`)
+        start: new Date(`${FRIDAY} 6:45`),
+        end: new Date(`${FRIDAY} 7:00`)
     },
     {
         name: "Work",
-        start: new Date(`${THURSDAY} 7:00`),
-        end: new Date(`${THURSDAY} 13:15`)
+        start: new Date(`${FRIDAY} 7:00`),
+        end: new Date(`${FRIDAY} 13:15`)
     },
     {
         name: "Lunch",
-        start: new Date(`${THURSDAY} 13:15`),
-        end: new Date(`${THURSDAY} 14:15`)
+        start: new Date(`${FRIDAY} 13:15`),
+        end: new Date(`${FRIDAY} 14:15`)
     },
     {
         name: "Clean up",
-        start: new Date(`${THURSDAY} 14:15`),
-        end: new Date(`${THURSDAY} 14:30`)
+        start: new Date(`${FRIDAY} 14:15`),
+        end: new Date(`${FRIDAY} 14:30`)
     },
     {
         name: "Detoxify",
-        start: new Date(`${THURSDAY} 14:30`),
-        end: new Date(`${THURSDAY} 15:00`)
+        start: new Date(`${FRIDAY} 14:30`),
+        end: new Date(`${FRIDAY} 15:00`)
     },
     {
         name: "Linux study",
-        start: new Date(`${THURSDAY} 15:00`),
-        end: new Date(`${THURSDAY} 21:00`)
+        start: new Date(`${FRIDAY} 15:00`),
+        end: new Date(`${FRIDAY} 17:00`)
+    },
+    {
+        name: "CT Exam",
+        start: new Date(`${FRIDAY} 17:00`),
+        end: new Date(`${FRIDAY} 18:30`)
+    },
+    {
+        name: "Uber business",
+        start: new Date(`${FRIDAY} 18:30`),
+        end: new Date(`${FRIDAY} 19:30`)
+    },
+    {
+        name: "Libellus server",
+        start: new Date(`${FRIDAY} 19:30`),
+        end: new Date(`${FRIDAY} 20:30`)
+    },
+    {
+        name: "Libellus deployment",
+        start: new Date(`${FRIDAY} 20:30`),
+        end: new Date(`${FRIDAY} 21:00`)
     },
     {
         name: "Libellus",
-        start: new Date(`${THURSDAY} 21:00`),
-        end: new Date(`${THURSDAY} 21:15`)
+        start: new Date(`${FRIDAY} 21:00`),
+        end: new Date(`${FRIDAY} 21:15`)
     },
     {
         name: "Setup",
-        start: new Date(`${THURSDAY} 21:45`),
-        end: new Date(`${THURSDAY} 22:00`)
+        start: new Date(`${FRIDAY} 21:45`),
+        end: new Date(`${FRIDAY} 22:00`)
     },
     {
         name: "Sleep",
-        start: new Date(`${THURSDAY} 22:00`),
-        end: new Date(`${FRIDAY} 6:45`)
+        start: new Date(`${FRIDAY} 22:00`),
+        end: new Date(`${SATURDAY} 6:45`)
     },
 ]
 
@@ -90,17 +115,7 @@ const MY_SCHEDULE = [
 
 
     // {
-    //     name: "CT Exam",
-    //     start: new Date(`${WEDNESDAY} 19:30`),
-    //     end: new Date(`${WEDNESDAY} 20:00`)
-    // },
-    // {
     //     name: "Call Brandon",
-    //     start: new Date(`${WEDNESDAY} 19:30`),
-    //     end: new Date(`${WEDNESDAY} 20:00`)
-    // },
-    // {
-    //     name: "Uber business",
     //     start: new Date(`${WEDNESDAY} 19:30`),
     //     end: new Date(`${WEDNESDAY} 20:00`)
     // },
@@ -108,16 +123,6 @@ const MY_SCHEDULE = [
     //     name: "Libellus refactor",
     //     start: new Date(`${} 13:30`),
     //     end: new Date(`${} 17:00`)
-    // },
-    // {
-    //     name: "Libellus deployment",
-    //     start: new Date(`${} 17:00`),
-    //     end: new Date(`${} 18:00`)
-    // },
-    // {
-    //     name: "Libellus server",
-    //     start: new Date(`${} `),
-    //     end: new Date(`${} `)
     // },
     // {
     //     name: "Web patterns",
