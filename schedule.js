97d1091778e8ebc3d6a29094a9a65115b7c42ac2
Justin Salcedo <nitsuj161214@gmail.com>
2022-12-28 13:57:44 -0800

    New content: 12/28-29 (Wednesday and Thursday)

--- schedule.js
+++ schedule.js
@@ -11,6 +11,7 @@
 //     end: new Date(`${}`)
 // },
 
+<<<<<<< Updated upstream
 const SATURDAY = "2022/12/17"
 const SUNDAY = "2022/12/18"
 const MONDAY = "2022/12/19"
@@ -18,10 +19,20 @@ const TUESDAY = "2022/12/20"
 const WEDNESDAY = "2022/12/21"
 const THURSDAY = "2022/12/22"
 const FRIDAY = "2022/12/23"
+=======
+const SATURDAY = "2022/12/24"
+const SUNDAY = "2022/12/25"
+const MONDAY = "2022/12/26"
+const TUESDAY = "2022/12/27"
+const WEDNESDAY = "2022/12/28"
+const THURSDAY = "2022/12/29"
+const FRIDAY = "2022/12/30"
+>>>>>>> Stashed changes
 // const SATURDAY = "2022/12/17"
 // const SUNDAY = "2022/12/18"
 
 const MY_SCHEDULE = [
+<<<<<<< Updated upstream
     {
         name: "Install CentOS",
         start: new Date(`${WEDNESDAY} 20:00`),
@@ -185,6 +196,99 @@ const MY_SCHEDULE = [
     //     name: "Sleep",
     //     start: new Date(`${FRIDAY} 22:00`),
     //     end: new Date(`${FRIDAY} 6:45`)
+=======
+
+    // Wednesday
+    {
+        name: "Shower",
+        start: new Date(`${WEDNESDAY} 15:45`),
+        end: new Date(`${WEDNESDAY} 16:15`)
+    },
+    // {
+    //     name: "Get ready",
+    //     start: new Date(`${WEDNESDAY} 17:15`),
+    //     end: new Date(`${WEDNESDAY} 17:45`)
+    // },
+    // {
+    //     name: "Bus stop",
+    //     start: new Date(`${WEDNESDAY} 17:45`),
+    //     end: new Date(`${WEDNESDAY} 18:00`)
+    // },
+    // {
+    //     name: "Take bus",
+    //     start: new Date(`${WEDNESDAY} 18:00`),
+    //     end: new Date(`${WEDNESDAY} 22:30`)
+    // },
+    // {
+    //     name: "Ride to airport",
+    //     start: new Date(`${WEDNESDAY} 22:45`),
+    //     end: new Date(`${WEDNESDAY} 23:30`)
+    // },
+    // {
+    //     name: "Quick dinner",
+    //     start: new Date(`${WEDNESDAY} 23:45`),
+    //     end: new Date(`${THURSDAY} 0:15`)
+    // },
+    // {
+    //     name: "Board",
+    //     start: new Date(`${THURSDAY} 0:45`),
+    //     end: new Date(`${THURSDAY} 1:45`)
+    // },
+    // {
+    //     name: "Flight",
+    //     start: new Date(`${THURSDAY} 1:45`),
+    //     end: new Date(`${THURSDAY} 3:00`)
+    // },
+    // {
+    //     name: "Unboard",
+    //     start: new Date(`${THURSDAY} 3:00`),
+    //     end: new Date(`${THURSDAY} 3:15`)
+    // },
+    // {
+    //     name: "Cross bridge",
+    //     start: new Date(`${THURSDAY} 3:15`),
+    //     end: new Date(`${THURSDAY} 3:30`)
+    // },
+    // {
+    //     name: "Ride home",
+    //     start: new Date(`${THURSDAY} 3:30`),
+    //     end: new Date(`${THURSDAY} 4:30`)
+    // },
+    // {
+    //     name: "Setup",
+    //     start: new Date(`${THURSDAY} 4:45`),
+    //     end: new Date(`${THURSDAY} 5:00`)
+    // },
+    // {
+    //     name: "Sleep",
+    //     start: new Date(`${THURSDAY} 5:00`),
+    //     end: new Date(`${THURSDAY} 11:00`)
+    // },
+    // {
+    //     name: "Meditate",
+    //     start: new Date(`${THURSDAY} 11:00`),
+    //     end: new Date(`${THURSDAY} 11:30`)
+    // },
+    // {
+    //     name: "Setup",
+    //     start: new Date(`${THURSDAY} 11:30`),
+    //     end: new Date(`${THURSDAY} 11:45`)
+    // },
+    // {
+    //     name: "Family time",
+    //     start: new Date(`${THURSDAY} 12:00`),
+    //     end: new Date(`${THURSDAY} 12:30`)
+    // },
+    // {
+    //     name: "Lunch",
+    //     start: new Date(`${THURSDAY} 12:30`),
+    //     end: new Date(`${THURSDAY} 13:00`)
+    // },
+    // {
+    //     name: "Libellus",
+    //     start: new Date(`${THURSDAY} 13:00`),
+    //     end: new Date(`${THURSDAY} 13:30`)
+>>>>>>> Stashed changes
     // },
 ]
 
@@ -192,7 +296,11 @@ const MY_SCHEDULE = [
 
 
     // {
+<<<<<<< Updated upstream
     //     name: "Prep Time Studies",
+=======
+    //     name: "Uber business",
+>>>>>>> Stashed changes
     //     start: new Date(`${WEDNESDAY} 19:30`),
     //     end: new Date(`${WEDNESDAY} 20:00`)
     // },
