de80f058b8e340120429f4d66313a32d6c71dda6
Justin Salcedo <nitsuj162114@gmail.com>
2023-01-02 00:31:08 -0800

    New content: 01/02 (Monday); template decomposition and new templates

--- generator.js
+++ generator.js
@@ -1,31 +1,225 @@
-const NOW = new Date('2023/01/01')
+const NOW = new Date('2023/01/02')
 const YESTERDAY = new Date(NOW.getTime() - 24 * 60 * 60 * 1000).toLocaleDateString('en-US')
 const TODAY = NOW.toLocaleDateString('en-US')
 const TOMORROW = new Date(NOW.getTime() + 24 * 60 * 60 * 1000).toLocaleDateString('en-US')
 
+const HEAD_SLEEP_LATE = [
+    {
+        name: "Dinner",
+        start: new Date(`${YESTERDAY} 21:30`),
+        end: new Date(`${YESTERDAY} 21:45`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${YESTERDAY} 21:45`),
+        end: new Date(`${YESTERDAY} 22:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${YESTERDAY} 22:00`),
+        end: new Date(`${YESTERDAY} 22:15`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${YESTERDAY} 23:00`),
+        end: new Date(`${TODAY} 7:45`)
+    }
+]
+
+const HEAD_SLEEP_EARLY = [
+    {
+        name: "Dinner",
+        start: new Date(`${YESTERDAY} 21:30`),
+        end: new Date(`${YESTERDAY} 21:45`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${YESTERDAY} 21:45`),
+        end: new Date(`${YESTERDAY} 22:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${YESTERDAY} 22:00`),
+        end: new Date(`${YESTERDAY} 22:15`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${YESTERDAY} 23:00`),
+        end: new Date(`${TODAY} 6:45`)
+    }
+]
+
+const HEAD_WAKEUP_LATE = [
+    {
+        name: "Meditate",
+        start: new Date(`${TODAY} 7:45`),
+        end: new Date(`${TODAY} 8:15`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${TODAY} 8:15`),
+        end: new Date(`${TODAY} 8:30`)
+    },
+    {
+        name: "Laundry/Clean up",
+        start: new Date(`${TODAY} 8:30`),
+        end: new Date(`${TODAY} 8:45`)
+    },
+    {
+        name: "(Get ahead)",
+        start: new Date(`${TODAY} 9:00`),
+        end: new Date(`${TODAY} 10:00`)
+    },
+    {
+        name: "Breakfast",
+        start: new Date(`${TODAY} 10:00`),
+        end: new Date(`${TODAY} 10:15`)
+    }
+]
+
+const HEAD_WORKDAY = [
+    {
+        name: "Meditate",
+        start: new Date(`${TODAY} 6:45`),
+        end: new Date(`${TODAY} 7:00`)
+    },
+    {
+        name: "Work",
+        start: new Date(`${TODAY} 7:00`),
+        end: new Date(`${TODAY} 13:15`)
+    },
+    {
+        name: "Lunch",
+        start: new Date(`${TODAY} 13:30`),
+        end: new Date(`${TODAY} 13:45`)
+    },
+    {
+        name: "Check email",
+        start: new Date(`${TODAY} 13:45`),
+        end: new Date(`${TODAY} 14:00`)
+    }
+]
+
+const HEAD_WORK_N_CLASS = [
+    ...HEAD_WORKDAY,
+    {
+        name: "Homework",
+        start: new Date(`${TODAY} 14:00`),
+        end: new Date(`${TODAY} 16:00`)
+    },
+    {
+        name: "PHYS204 live lesson",
+        start: new Date(`${TODAY} 16:00`),
+        end: new Date(`${TODAY} 17:00`)
+    }
+]
+
+const TAIL_SLEEP_EARLY = [
+    {
+        name: "Dinner",
+        start: new Date(`${TODAY} 21:30`),
+        end: new Date(`${TODAY} 21:45`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${TODAY} 21:45`),
+        end: new Date(`${TODAY} 22:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${TODAY} 22:00`),
+        end: new Date(`${TODAY} 22:15`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${TODAY} 23:00`),
+        end: new Date(`${TOMORROW} 6:45`)
+    }
+]
+
+const TAIL_SLEEP_LATE = [
+    {
+        name: "Dinner",
+        start: new Date(`${TODAY} 21:30`),
+        end: new Date(`${TODAY} 21:45`)
+    },
+    {
+        name: "Libellus",
+        start: new Date(`${TODAY} 21:45`),
+        end: new Date(`${TODAY} 22:00`)
+    },
+    {
+        name: "Setup",
+        start: new Date(`${TODAY} 22:00`),
+        end: new Date(`${TODAY} 22:15`)
+    },
+    {
+        name: "Sleep",
+        start: new Date(`${TOMORROW} 0:00`),
+        end: new Date(`${TOMORROW} 7:45`)
+    }
+]
+
+/* Templates */
+
 const WEEKDAY = {
     head: [
         // Yesterday
-        {
-            name: "Dinner",
-            start: new Date(`${YESTERDAY} 21:30`),
-            end: new Date(`${YESTERDAY} 21:45`)
-        },
-        {
-            name: "Libellus",
-            start: new Date(`${YESTERDAY} 21:45`),
-            end: new Date(`${YESTERDAY} 22:00`)
-        },
-        {
-            name: "Setup",
-            start: new Date(`${YESTERDAY} 22:00`),
-            end: new Date(`${YESTERDAY} 22:15`)
-        },
-        {
-            name: "Sleep",
-            start: new Date(`${YESTERDAY} 23:00`),
-            end: new Date(`${TODAY} 6:45`)
-        },
+        ...HEAD_SLEEP_EARLY,
+
+        // Today
+        ...HEAD_WORKDAY
+    ],
+    tail: [
+        // Today
+        ...TAIL_SLEEP_EARLY
+    ]
+}
+
+const TUESDAY = {
+    ...WEEKDAY,
+    head: [
+        // Yesterday
+        ...HEAD_SLEEP_EARLY,
+
+        // Today
+        ...HEAD_WORK_N_CLASS
+    ],
+}
+
+const FRIDAY = {
+    ...WEEKDAY,
+    tail: [
+        ...TAIL_SLEEP_LATE
+    ]
+}
+
+const WEEKEND = {
+    head: [
+        // Yesterday
+        ...HEAD_SLEEP_LATE,
+
+        // Today
+        ...HEAD_WAKEUP_LATE
+    ],
+    tail: [
+        // Today
+        ...TAIL_SLEEP_LATE
+    ]
+}
+
+const SUNDAY = {
+    ...WEEKEND,
+    tail: [
+        ...TAIL_SLEEP_EARLY
+    ]
+}
+
+const CUSTOMDAY = {
+    ...WEEKDAY,
+    head: [
+        // Yesterday
+        ...HEAD_SLEEP_EARLY,
 
         // Today
         {
@@ -34,8 +228,18 @@ const WEEKDAY = {
             end: new Date(`${TODAY} 7:00`)
         },
         {
-            name: "Work",
+            name: "Setup",
             start: new Date(`${TODAY} 7:00`),
+            end: new Date(`${TODAY} 7:15`)
+        },
+        {
+            name: "Breakfast",
+            start: new Date(`${TODAY} 7:15`),
+            end: new Date(`${TODAY} 7:30`)
+        },
+        {
+            name: "CompTIA Linux+",
+            start: new Date(`${TODAY} 8:00`),
             end: new Date(`${TODAY} 13:15`)
         },
         {
@@ -47,106 +251,7 @@ const WEEKDAY = {
             name: "Check email",
             start: new Date(`${TODAY} 13:45`),
             end: new Date(`${TODAY} 14:00`)
-        },
-    ],
-    tail: [
-        // Today
-        {
-            name: "Dinner",
-            start: new Date(`${TODAY} 21:30`),
-            end: new Date(`${TODAY} 21:45`)
-        },
-        {
-            name: "Libellus",
-            start: new Date(`${TODAY} 21:45`),
-            end: new Date(`${TODAY} 22:00`)
-        },
-        {
-            name: "Setup",
-            start: new Date(`${TODAY} 22:00`),
-            end: new Date(`${TODAY} 22:15`)
-        },
-        {
-            name: "Sleep",
-            start: new Date(`${TODAY} 23:00`),
-            end: new Date(`${TOMORROW} 6:45`)
-        },
-    ]
-}
-
-const WEEKEND = {
-    head: [
-        // Yesterday
-        {
-            name: "Dinner",
-            start: new Date(`${YESTERDAY} 21:30`),
-            end: new Date(`${YESTERDAY} 21:45`)
-        },
-        {
-            name: "Libellus",
-            start: new Date(`${YESTERDAY} 21:45`),
-            end: new Date(`${YESTERDAY} 22:00`)
-        },
-        {
-            name: "Setup",
-            start: new Date(`${YESTERDAY} 22:00`),
-            end: new Date(`${YESTERDAY} 22:15`)
-        },
-        {
-            name: "Sleep",
-            start: new Date(`${YESTERDAY} 23:00`),
-            end: new Date(`${TODAY} 7:45`)
-        },
-
-        // Today
-        {
-            name: "Meditate",
-            start: new Date(`${TODAY} 7:45`),
-            end: new Date(`${TODAY} 8:15`)
-        },
-        {
-            name: "Setup",
-            start: new Date(`${TODAY} 8:15`),
-            end: new Date(`${TODAY} 8:30`)
-        },
-        {
-            name: "Laundry/Clean up",
-            start: new Date(`${TODAY} 8:30`),
-            end: new Date(`${TODAY} 8:45`)
-        },
-        {
-            name: "(Get ahead)",
-            start: new Date(`${TODAY} 9:00`),
-            end: new Date(`${TODAY} 10:00`)
-        },
-        {
-            name: "Breakfast",
-            start: new Date(`${TODAY} 10:00`),
-            end: new Date(`${TODAY} 10:15`)
-        },
-    ],
-    tail: [
-        // Today
-        {
-            name: "Dinner",
-            start: new Date(`${TODAY} 21:30`),
-            end: new Date(`${TODAY} 21:45`)
-        },
-        {
-            name: "Libellus",
-            start: new Date(`${TODAY} 21:45`),
-            end: new Date(`${TODAY} 22:00`)
-        },
-        {
-            name: "Setup",
-            start: new Date(`${TODAY} 22:00`),
-            end: new Date(`${TODAY} 22:15`)
-        },
-        {
-            name: "Sleep",
-            start: new Date(`${TODAY} 23:00`),
-            end: new Date(`${TOMORROW} 6:45`)
-        },
+        }
     ]
 }
 
@@ -214,19 +319,19 @@ function getTimespan(day) {
 
 function getTemplate(day) {
     let template = null
-    if (day === "weekday") {
-        template = WEEKDAY
-    }
 
-    if (day === "weekend") {
-        template = WEEKEND
-    }
+    if (day === "weekday") template = WEEKDAY
+    if (day === "tuesday") template = TUESDAY
+    if (day === "friday") template = FRIDAY
+    if (day === "weekend") template = WEEKEND
+    if (day === "sunday") template = SUNDAY
+    if (day === "custom") template = CUSTOMDAY
 
     if (!template) throw new Error('No valid template selected')
 
     return template
 }
 
-mySchedule = generate('weekend', 'Boil water', 30, 'CompTIA Linux+', 3 * 60, '', 15, 'Online order', 15, 'Launch', 30, 'CompTIA Linux+', 3 * 60, 'Photo session', 15, '', 30, 'CompTIA Linux+', 2 * 60)
+mySchedule = generate('custom', 'Walk out', 15, 'Setup', 15, 'Fit test', 30, 'Setup', 15, 'Call mechanic', 15, 'AP Prep quiz', 15, 'AP Tutorials', 15, 'AP Knowledge check', 15, 'AP WebAssign', 60, '', 15, 'AP Discussion', 60, '', 15, 'CT Poll', 15, 'CT PlayPosit', 15, 'CT Mindtap', 60, 'CT Quiz', 15, 'CT Discussion', 60)
 clearInterval(interval)
 interval = setInterval(() => loadContent(getTaskQueue(mySchedule)), 1000)
--- schedule.js
+++ schedule.js
@@ -11,64 +11,70 @@
 //     end: new Date(`${}`)
 // },
 
-const SATURDAY = "2022/12/24"
-const SUNDAY = "2022/12/25"
-const MONDAY = "2022/12/26"
-const TUESDAY = "2022/12/27"
-const WEDNESDAY = "2022/12/28"
-const THURSDAY = "2022/12/29"
-const FRIDAY = "2022/12/30"
+// const SATURDAY = "2022/12/24"
+// const SUNDAY = "2022/12/25"
+// const MONDAY = "2022/12/26"
+// const TUESDAY = "2022/12/27"
+// const WEDNESDAY = "2022/12/28"
+// const THURSDAY = "2022/12/29"
+// const FRIDAY = "2022/12/30"
 // const SATURDAY = "2022/12/17"
 // const SUNDAY = "2022/12/18"
 
 const MY_SCHEDULE = [
+    // placeholder
+    {
+        name: "Far future",
+        start: new Date('2025/06/12 4:00'),
+        end: new Date('2025/06/12 16:00')
+    }
 
     // Thursday
-    {
-        name: "Priority mesh",
-        start: new Date(`${THURSDAY} 14:00`),
-        end: new Date(`${THURSDAY} 14:45`)
-    },
-    {
-        name: "Libellus",
-        start: new Date(`${THURSDAY} 14:45`),
-        end: new Date(`${THURSDAY} 15:00`)
-    },
-    {
-        name: "ScheduleGen",
-        start: new Date(`${THURSDAY} 15:15`),
-        end: new Date(`${THURSDAY} 18:30`)
-    },
-    {
-        name: "Movie time!",
-        start: new Date(`${THURSDAY} 18:30`),
-        end: new Date(`${THURSDAY} 20:30`)
-    },
-    {
-        name: "ScheduleGen",
-        start: new Date(`${THURSDAY} 20:30`),
-        end: new Date(`${THURSDAY} 21:00`)
-    },
-    {
-        name: "Libellus",
-        start: new Date(`${THURSDAY} 21:00`),
-        end: new Date(`${THURSDAY} 21:30`)
-    },
-    {
-        name: "Dinner",
-        start: new Date(`${THURSDAY} 21:30`),
-        end: new Date(`${THURSDAY} 21:45`)
-    },
-    {
-        name: "Setup",
-        start: new Date(`${THURSDAY} 22:00`),
-        end: new Date(`${THURSDAY} 22:15`)
-    },
-    {
-        name: "Sleep",
-        start: new Date(`${THURSDAY} 23:00`),
-        end: new Date(`${FRIDAY} 6:45`)
-    },
+    // {
+    //     name: "Priority mesh",
+    //     start: new Date(`${THURSDAY} 14:00`),
+    //     end: new Date(`${THURSDAY} 14:45`)
+    // },
+    // {
+    //     name: "Libellus",
+    //     start: new Date(`${THURSDAY} 14:45`),
+    //     end: new Date(`${THURSDAY} 15:00`)
+    // },
+    // {
+    //     name: "ScheduleGen",
+    //     start: new Date(`${THURSDAY} 15:15`),
+    //     end: new Date(`${THURSDAY} 18:30`)
+    // },
+    // {
+    //     name: "Movie time!",
+    //     start: new Date(`${THURSDAY} 18:30`),
+    //     end: new Date(`${THURSDAY} 20:30`)
+    // },
+    // {
+    //     name: "ScheduleGen",
+    //     start: new Date(`${THURSDAY} 20:30`),
+    //     end: new Date(`${THURSDAY} 21:00`)
+    // },
+    // {
+    //     name: "Libellus",
+    //     start: new Date(`${THURSDAY} 21:00`),
+    //     end: new Date(`${THURSDAY} 21:30`)
+    // },
+    // {
+    //     name: "Dinner",
+    //     start: new Date(`${THURSDAY} 21:30`),
+    //     end: new Date(`${THURSDAY} 21:45`)
+    // },
+    // {
+    //     name: "Setup",
+    //     start: new Date(`${THURSDAY} 22:00`),
+    //     end: new Date(`${THURSDAY} 22:15`)
+    // },
+    // {
+    //     name: "Sleep",
+    //     start: new Date(`${THURSDAY} 23:00`),
+    //     end: new Date(`${FRIDAY} 6:45`)
+    // },
 ]
 
 // Task pool
