ffbf5af31c1a0391082d5ccbba886ec60bd40ead
Justin Salcedo <nitsuj162114@gmail.com>
2023-01-22 23:20:36 -0800

    New content: 01/23 (Monday)

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-const NOW = new Date('2023/01/22')
+const NOW = new Date('2023/01/23')
 const YESTERDAY = new Date(NOW.getTime() - 24 * 60 * 60 * 1000).toLocaleDateString('en-US')
 const TODAY = NOW.toLocaleDateString('en-US')
 const TOMORROW = new Date(NOW.getTime() + 24 * 60 * 60 * 1000).toLocaleDateString('en-US')
@@ -359,6 +359,6 @@ function getTemplate(day) {
     return template
 }
 
-mySchedule = generate('sunday', 'Restore JS Website',  45, 'CT PlayPosit', 15, 'CT Poll', 15, 'CT MindTap', 15, 'CT Prep quiz', 15, 'CT Quiz', 15, '', 15, 'AP Project', 2 * 60, 'CompTIA Linux+', 90, ``, 15, 'CompTIA Linux+', 90, ``, 15, '', 15, 'CompTIA Linux+', 90, ``, 15, 'CompTIA Linux+', 90)
+mySchedule = generate('weekday', 'Restore JS website', 15, 'AP Knowledge check', 15, 'AP Project', 90, '', 15, 'CompTIA Linux+', 90, '', 15, 'Pure Cardio & Cardio Abs', 90, 'Shower', 30, '', 15, 'AP Discussion', 60)
 clearInterval(interval)
 interval = setInterval(() => loadContent(getTaskQueue(mySchedule)), 1000)
