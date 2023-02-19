d9937efa8e9375c5bcaff442e0bd416b2e0ab5b5
Justin Salcedo <nitsuj162114@gmail.com>
2023-02-19 00:28:42 -0800

    New content: 02/19 (Sunday)

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-const NOW = new Date('2023/02/18')
+const NOW = new Date('2023/02/19')
 const YESTERDAY = new Date(NOW.getTime() - 24 * 60 * 60 * 1000).toLocaleDateString('en-US')
 const TODAY = NOW.toLocaleDateString('en-US')
 const TOMORROW = new Date(NOW.getTime() + 24 * 60 * 60 * 1000).toLocaleDateString('en-US')
@@ -358,11 +358,8 @@ const SUNDAY = {
 }
 
 const CUSTOMDAY = {
-    head: [
-        ...SLEPT_EARLY,
-        ...WORKDAY
-    ],
-    tail: SLEEP_LATE
+    ...WEEKEND,
+    tail: SLEEP_EARLY
 }
 
 function generate(day, ...taskList) {
@@ -446,7 +443,7 @@ function getTemplate(day) {
     return template
 }
 
-mySchedule = generate('saturday', 'Shower and shave', 45, '', 15, 'Drive', 60, 'Visit aunt', 90, 'Drive', 60, '', 15, 'Online shopping', 30, 'Flight tickets', 30, 'Activate gift card', 15, 'Reschedule exam', 15, '', 15, 'CompTIA Linux+', 90, '', 15, 'CompTIA Linux+', 90, '', 15, 'CompTIA Linux+', 60)
+mySchedule = generate('custom', 'AP Knowledge check', 15, 'AP Nearpod', 30, 'CT Mindtap', 30, '', 15, 'CT Discussion', 45, '', 15, 'Shower', 45, '', 15, 'Online shopping', 30, 'Flight tickets', 30, 'Salad time!', 15, 'Drive', 30, 'Errands', 2 * 60, 'Drive', 30, '', 15, 'Activate gift card', 15, 'Reschedule exam', 15, 'CompTIA Linux+', 90, '', 15, 'CompTIA Linux+', 45)
 // setScheduleForLb(mySchedule).then(savedSchedule => console.log(savedSchedule))
 clearInterval(interval)
 interval = setInterval(() => loadContent(getTaskQueue(mySchedule)), 1000)
