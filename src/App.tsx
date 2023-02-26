86c36eec8e51c6b7efa779bbf116e85b589c6621
Justin Salcedo <nitsuj162114@gmail.com>
2023-02-25 19:40:17 -0800

    Load schedule from DB. Save fetched schedule to local storage to use when reloading.
    - Add hasLoaded status to get schedule from local storage or fetch fresh records at useEffect()
    - formatSchedule() from JSON-parsed schedule to load Date objects
    
    TODO:
    - Hide "+" (add) button for schedule form
    - Implement functions form "content" branch
    - Reload schedule from form
    - Add schedule with form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import './styles/global.css'
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import Schedule from "./api/Schedule"
 import MainScreen from "./pages/MainScreen"
 import { ScheduleContext } from './contexts'
 import { timelineIncludesToday, validateSchedule } from './utils'
-import MY_SCHEDULE from './constants/schedule'
 import ScheduleComplete from './pages/ScheduleComplete'
+import { USER_ID } from './constants'
+import { ITask } from './types'
 
 const App = () => {
-    const [schedule, setSchedule] = useState(validateSchedule(MY_SCHEDULE))
+    const [schedule, setSchedule] = useState([])
+    const [hasLoaded, setHasLoaded] = useState(false)
 
     // TODO: Get schedule from server
+    useEffect(() => {
+        if (!hasLoaded) {
+            const localSchedule = window.localStorage.getItem('schedule')
+            if (localSchedule) {
+                setSchedule(validateSchedule(formatSchedule(JSON.parse(localSchedule))))
+                setHasLoaded(true)
+            } else {
+                const scheduleApi = new Schedule(USER_ID)
+                scheduleApi.Get()
+                    .then(data => {
+                        setSchedule(validateSchedule(data))
+                        window.localStorage.setItem('schedule', JSON.stringify(data))
+                    })
+                    .catch(e => console.error(e))
+                    .finally(() => setHasLoaded(true))
+            }
+        }
+    }, [hasLoaded])
 
     const isActiveSchedule = () => schedule.length && timelineIncludesToday(schedule)
 
     return (
         <ScheduleContext.Provider value={{ schedule, setSchedule }}>
-            {isActiveSchedule() ? <MainScreen/> : <ScheduleComplete/>}
-            
+            {!hasLoaded ? "Loading..." : (isActiveSchedule() ? <MainScreen/> : <ScheduleComplete/>)} 
         </ScheduleContext.Provider>
     )
+}
+
+function formatSchedule(localSchedule: ITask[]) {
+    return localSchedule.map(task => ({ ...task, start: new Date(task.start), end: new Date(task.end) }))
 }
 
 export default App
