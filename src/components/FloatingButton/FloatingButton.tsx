b2289e5582034c47e68c69ac75b62f78b571e72f
Justin Salcedo <nitsuj162114@gmail.com>
2023-02-25 20:34:02 -0800

    Hide button and show it after three clicks/taps. Hide again after modal was loaded

--- src/components/FloatingButton/FloatingButton.tsx
+++ src/components/FloatingButton/FloatingButton.tsx
@@ -1,18 +1,24 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { ViewContext } from "../../contexts"
 import styles from "./FloatingButton.module.css"
 
 export default function FloatingButton() {
     const { setActiveModal, launchModal } = useContext(ViewContext)
+    const [clicks, setClicks] = useState(0)
 
-    function handleOnClick() {
+    function handleClickOnArea() {
+        if (clicks < 3) setClicks(clicks => clicks + 1)
+    }
+
+    function handleClickOnButton() {
         setActiveModal('schedule-form')
         launchModal(true)
+        setClicks(0)
     }
 
     return (
-        <div className={styles.area} onClick={handleOnClick}>
-            <div className={styles.circle}><span>+</span></div>
+        <div className={styles.area} onClick={handleClickOnArea}>
+            {clicks > 2 ? <div className={styles.circle} onClick={handleClickOnButton}><span>+</span></div> : ''}
         </div>
     )
 }
