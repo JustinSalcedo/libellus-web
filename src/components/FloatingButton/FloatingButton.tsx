6529c43eb2f5221f59713ea976d5e786697342db
Justin Salcedo <nitsuj162114@gmail.com>
2023-02-26 17:32:17 -0800

    Add refresh button to new actions tray

--- src/components/FloatingButton/FloatingButton.tsx
+++ src/components/FloatingButton/FloatingButton.tsx
@@ -1,24 +1,35 @@
 import React, { useContext, useState } from "react"
-import { ViewContext } from "../../contexts"
+import { ScheduleContext, ViewContext } from "../../contexts"
 import styles from "./FloatingButton.module.css"
 
 export default function FloatingButton() {
     const { setActiveModal, launchModal } = useContext(ViewContext)
+    const { refreshSchedule } = useContext(ScheduleContext)
     const [clicks, setClicks] = useState(0)
 
     function handleClickOnArea() {
         if (clicks < 3) setClicks(clicks => clicks + 1)
     }
 
-    function handleClickOnButton() {
+    function handleClickOnPlus() {
         setActiveModal('schedule-form')
         launchModal(true)
         setClicks(0)
     }
 
+    function handleClickOnReload() {
+        refreshSchedule()
+        setClicks(0)
+    }
+
     return (
-        <div className={styles.area} onClick={handleClickOnArea}>
-            {clicks > 2 ? <div className={styles.circle} onClick={handleClickOnButton}><span>+</span></div> : ''}
+        <div className={styles.tray}>
+            <div className={styles.area} onClick={handleClickOnArea}>
+                {clicks > 2 ? <div className={styles.circle} onClick={handleClickOnPlus}><span>+</span></div> : ''}
+            </div>
+            <div className={styles.area}>
+                {clicks > 2 ? <div className={styles.circle} onClick={handleClickOnReload}><span style={{ height: '6rem' }}>⟳</span></div> : ''}
+            </div>
         </div>
     )
 }
